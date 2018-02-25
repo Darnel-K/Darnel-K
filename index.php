d4e3416bef3cf9a4c0686a64813fe8b21fa14e35
Darnel-K <darnel.kumar91@gmail.com>
2018-02-25 20:35:16 +0000

    -

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require_once 'PHP-Includes/ClassAutoload.php';
+        $t = new test();
+        $t->test();
+    ?>
+</body>
+</html>
