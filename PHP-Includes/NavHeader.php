c3c7f8e36145c59e03627d84c1db1d393a5b06c9
Darnel-K <darnel.kumar91@gmail.com>
2018-02-25 23:32:18 +0000

    -

--- PHP-Includes/NavHeader.php
+++ PHP-Includes/NavHeader.php
@@ -2,5 +2,5 @@
 $HeaderBar = "<header><nav>";
 $Nav = new Navigation($NavLinks);
 $HeaderBar .= $Nav->GenNavHTML();
-$HeaderBar .= "<img src='Images/Logo - White Text.svg' alt='Logo SVG'></nav></header>";
+$HeaderBar .= "</nav><img src='Images/Logo - White Text.svg' alt='Logo SVG'></header>";
 ?>
