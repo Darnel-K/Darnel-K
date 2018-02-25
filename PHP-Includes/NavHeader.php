c119157bd232d30abab7133837ddbd4007849f02
Darnel-K <darnel.kumar91@gmail.com>
2018-02-25 23:34:33 +0000

    -

--- PHP-Includes/NavHeader.php
+++ PHP-Includes/NavHeader.php
@@ -1,6 +1,6 @@
 <?php
-$HeaderBar = "<header><nav>";
+$HeaderBar = "<header><div id='particle-js'></div><div id='overlay'><nav>";
 $Nav = new Navigation($NavLinks);
 $HeaderBar .= $Nav->GenNavHTML();
-$HeaderBar .= "</nav><img src='Images/Logo - White Text.svg' alt='Logo SVG'></header>";
+$HeaderBar .= "</nav><img src='Images/Logo - White Text.svg' alt='Logo SVG'></div></header>";
 ?>
