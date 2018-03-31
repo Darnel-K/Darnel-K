9dc2f2919eadd48096e51d2bd46465dd4519c12b
Darnel Kumar <darnel.kumar91@gmail.com>
2018-03-31 22:15:38 +0100

    -

--- Scripts/PHP/SubmitFingerprint.php
+++ Scripts/PHP/SubmitFingerprint.php
@@ -25,9 +25,27 @@
 
     if (isset($_POST['FP'])) {
         $Data['FP'] = mysqli_real_escape_string($CONN['Connection'], $_POST['FP']);
+    } else {
+        $output['Error'] = 'Fingerprint ID Not Received!';
+        EncodeAndExit($output);
     }
     if (isset($_POST['FP_Data'])) {
         $Data['FP_Data'] = $_POST['FP_Data'];
+    } else {
+        $output['Error'] = 'Fingerprint Data Not Received!';
+        EncodeAndExit($output);
+    }
+
+    foreach ($Data['FP_Data'] as $k => $v) {
+        if (is_array($v)) {
+            $arr = array();
+            foreach ($v as $va) {
+                array_push($arr, mysqli_real_escape_string($CONN['Connection'], $va));
+            }
+            $Data['FP_Data'][$k] = $arr;
+        } else {
+            $Data['FP_Data'][$k] = mysqli_real_escape_string($CONN['Connection'], $v);
+        }
     }
 
     $output['Data'] = $Data;
