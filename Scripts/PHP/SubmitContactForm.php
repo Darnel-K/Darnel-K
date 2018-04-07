c41b6ca04c26b09537cea510ffb9199c366614c5
Darnel-K <darnel.kumar91@gmail.com>
2018-04-07 01:21:14 +0100

    -

--- Scripts/PHP/SubmitContactForm.php
+++ Scripts/PHP/SubmitContactForm.php
@@ -8,7 +8,8 @@
         "URL" => "https://hooks.slack.com/services/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",
         "Color" => '#2F2F2F',
         "Date" => date("Y-m-d"),
-        "Time" => date("h:i:s A T")
+        "Time" => date("h:i:s A T"),
+        "Sent" => 0
     );
 
     function EncodeAndExit($out) { echo json_encode($out); exit(); }
@@ -75,22 +76,19 @@
         'Content-Length: ' . strlen($SlackData))
     );
 
-    $output["Data"] = curl_exec($ch);
+    $result = curl_exec($ch);
 
-    // $SQL = "SELECT ID, FingerprintID FROM Fingerprint_Data WHERE FingerprintID = '{$Data['FP']}'";
-    // if ($CONN['Connection']->query($SQL)->num_rows <= 0) {
-    //     $Data['FP_Data']['Date'] = date("Y-m-d");
-    //     $SQL = "INSERT INTO Fingerprint_Data (FingerprintID, User_Agent, Language, Color_Depth, Pixel_Ratio, Hardware_Concurrency, Resolution, Available_Resolution, Timezone_Offset, CPU_Class, Navigator_Platform, Regular_Plugins, Canvas, WebGL, AdBlock, Has_Lied_Languages, Has_Lied_Resolution, Has_Lied_OS, Has_Lied_Browser, Touch_Support, JS_Fonts, First_Visit_Date, WebGL_Vendor, Do_Not_Track) VALUES ('{$Data['FP']}', '{$Data['FP_Data']['user_agent']}', '{$Data['FP_Data']['language']}', '{$Data['FP_Data']['color_depth']}', '{$Data['FP_Data']['pixel_ratio']}', '{$Data['FP_Data']['hardware_concurrency']}', '{$Data['FP_Data']['resolution']}', '{$Data['FP_Data']['available_resolution']}', '{$Data['FP_Data']['timezone_offset']}', '{$Data['FP_Data']['cpu_class']}', '{$Data['FP_Data']['navigator_platform']}', '{$Data['FP_Data']['regular_plugins']}', '{$Data['FP_Data']['canvas']}', '{$Data['FP_Data']['webgl']}', '{$Data['FP_Data']['adblock']}', '{$Data['FP_Data']['has_lied_languages']}', '{$Data['FP_Data']['has_lied_resolution']}', '{$Data['FP_Data']['has_lied_os']}', '{$Data['FP_Data']['has_lied_browser']}', '{$Data['FP_Data']['touch_support']}', '{$Data['FP_Data']['js_fonts']}', '{$Data['FP_Data']['Date']}', '{$Data['FP_Data']['webgl_vendor']}', '{$Data['FP_Data']['do_not_track']}')";
-    //     if ($CONN['Connection']->query($SQL) === TRUE) {
-    //         $output['Error'] = null;
-    //         $output['Data'] = 'Fingerprint Stored Successfully';
-    //     } else {
-    //         $output['Error'] = $CONN['Connection']->error;
-    //         $output['Data'] = $Data['FP'];
-    //     }
-    // } else {
-    //     $output['Error'] = 'Fingerprint ID: ' . $Data['FP'] . ' Already Found!';
-    //     $output['Data'] = $Data['FP'];
-    // }
+    if ($result) {
+        $SLACK['Sent'] = 1;
+    }
+
+    $SQL = "INSERT INTO Contact_Submissions (FUll_Name, Email, Subject, MSG, Slack_Sent, Date_Added) VALUES ('{$Data['FName']}', '{$Data['Email']}', '{$Data['Subject']}', '{$Data['MSG']}', '{$SLACK['Sent']}', '{$SLACK['Date']}')";
+    if ($CONN['Connection']->query($SQL) === TRUE) {
+        $output['Error'] = ($SLACK['Sent'] == 0 ? 'Unable to send to slack, will try again later.' : null);
+        $output['Data'] = ($SLACK['Sent'] == 1 ? 'Message stored & sent to slack successfully.' : 'Message stored but not sent to slack.');
+    } else {
+        $output['Error'] = ($SLACK['Sent'] == 0 ? 'Something went wrong, your message was not stored or sent to slack.' : 'Something went wrong, your message could not be stored but has been sent to slack.');
+        $output['Data'] = null;
+    }
     EncodeAndExit($output);
 ?>
