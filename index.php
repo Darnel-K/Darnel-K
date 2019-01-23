bdbb48d4b00985d955d14fb73ab6423a937b4d96
Darnel Kumar <darnel.kumar91@gmail.com>
2019-01-23 02:03:10 +0000

    Added Under Construction Page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSS/Main.min.css">
+    <link rel="stylesheet" href="CSS/Dark.min.css">
+    <title>Darnel-K | Website Under Construction!</title>
+</head>
+<body>
+    <div id="Container">
+        <div class="swing">
+            <h1>Under Construction!</h1>
+            <section>
+                <h3>Just in case you were wondering...</h3>
+                <p>This website is under construction, please check back later. If you wish to view the development version of this website you can do so at this link, <a href="https://dev.darnel-k.uk">https://dev.darnel-k.uk</a>.</p>
+            </section>
+        </div>
+    </div>
+    <footer><p id='Copyright'>Copyright &copy; <?php echo date('Y'); ?> Darnel-K</p></footer>
+</body>
+</html>
